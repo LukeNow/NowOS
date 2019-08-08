9645fc20f93143b4bd23fe3ff7577af788733da4
Luke Nowakowski-Krijger <lnowakow@eng.ucsd.edu>
2019-08-08 03:11:29 -0700

    refactor mm to use klib and implement page kmalloc

--- kernel/kheap.c
+++ kernel/kheap.c
@@ -5,17 +5,20 @@
 #include "../include/kdef.h"
 #include "../include/sorted_array_list.h"
 #include "../include/string.h"
+#include "../include/byte_index_list.h"
+#include "../include/kdef.h"
 
 #define HEAP_EXPAND_SIZE KB(4)
 #define SECTOR_SIZE 512
 #define HEAP_SPLIT_THRESHOLD 16
-
 #define MAGIC_FREE 0x45455246 //FREE IN ASCII
 #define MAGIC_USED 0x44455355 //USED IN ASCII
 #define USED 1
 #define FREE 0
-
 #define HEADER_SIZE sizeof(node_header_t)
+
+BYTE_INDEX_LIST_INIT(page_list);
+SORTED_ARRAY_LIST_INIT(free_node_list);
 
 extern void heap_bottom();
 extern void heap_top();
@@ -28,13 +31,13 @@ uint32_t heap_ptr = 0;
 uint32_t top_heap_ptr = 0;
 uint32_t early_heap_ptr = 0;
 uint32_t heap_list_end_ptr = 0;
+uint32_t page_ptr = 0;
 
-sorted_array_list_t free_list;
 
 /*********************
  * Early kheap methods
  *********************/
-void init_early_kheap()
+void init_early_heap()
 {
 	early_heap_ptr = (uint32_t) early_heap_bottom;
 	kprint(INFO, "[INIT EARLY KHEAP] Early heap ptr: %x\n", early_heap_ptr);
@@ -139,11 +142,11 @@ static void heap_print_list()
 
 static node_header_t *heap_find_free_node(uint32_t alloc_size)
 {
-	unsigned int list_len = free_list.curr_size - 1;
+	unsigned int list_len = free_node_list.curr_size - 1;
 	node_header_t *best_node = NULL;
 	
 	for (int i = 0; i < list_len; i++) {
-		node_header_t *node = (node_header_t *)free_list.arr[i];
+		node_header_t *node = (node_header_t *)free_node_list.arr[i];
 		/* Santity check */
 		check_header(node);
 		
@@ -215,19 +218,19 @@ static void heap_merge_nodes(node_header_t *node)
 	node_header_t *merge_node = node;
 
 	if (node_right != NULL && node_right->magic == MAGIC_FREE) {
-		remove_list((void *)node, &free_list);
-		remove_list((void *)node_right, &free_list);
+		remove_list((void *)node, &free_node_list);
+		remove_list((void *)node_right, &free_node_list);
 		merge_node = heap_merge_right(node);
 	}
 	if (node_left != NULL && node_left->magic == MAGIC_FREE){
-		remove_list((void *)node, &free_list);
-		remove_list((void *)node_left, &free_list);
+		remove_list((void *)node, &free_node_list);
+		remove_list((void *)node_left, &free_node_list);
 		merge_node = heap_merge_left(node);
 	}
 	
 	/* If nothing is merged, then the node is added back
 	 * to free list */
-	insert_list((void *)merge_node, &free_list);
+	insert_list((void *)merge_node, &free_node_list);
 }
 
 static void heap_split_node(node_header_t *free_node, uint32_t new_alloc_size)
@@ -240,7 +243,7 @@ static void heap_split_node(node_header_t *free_node, uint32_t new_alloc_size)
 	
 	/* Lets remove this node from the list to make
 	 * new ones. */
-	remove_list((void *)free_node, &free_list);
+	remove_list((void *)free_node, &free_node_list);
 
 	node_header_t *old_next_ptr = free_node->next_ptr;
 	node_header_t *old_prev_ptr = free_node->prev_ptr;
@@ -301,7 +304,7 @@ static void heap_split_node(node_header_t *free_node, uint32_t new_alloc_size)
 			old_prev_ptr, new_alloc_size);
 	
 	/* Add our new free node to the free list */
-	insert_list((void *)new_free_node, &free_list);
+	insert_list((void *)new_free_node, &free_node_list);
 
 	/* If this is the last node in the list, set the last ptr */
 	if (new_free_node->next_ptr == NULL)
@@ -329,7 +332,7 @@ static void expand_heap(size_t size)
 	last_node->next_ptr = (node_header_t *)new_free_node;
 	
 	/* Insert our new expanded node to the list of free nodes */
-	insert_list((void *)new_free_node, &free_list);
+	insert_list((void *)new_free_node, &free_node_list);
 	
 	/* Merge the nodes around our new node to make one big free node */
 	heap_merge_nodes((node_header_t *)new_free_node);
@@ -390,7 +393,7 @@ void kfree(void *ptr)
 	heap_merge_nodes(node);
 }
 
-void check_heap_integrity()
+void check_kheap_integrity()
 {
 	
 	uint32_t heap_size = (uint32_t)top_heap_ptr - (uint32_t)heap_bottom;
@@ -416,16 +419,14 @@ void init_kheap()
 	
 	uint32_t total_heap_size = (uint32_t)heap_top - (uint32_t)heap_bottom;
 	memset((void *)heap_ptr, 0, total_heap_size);
-
 	/* Align just in case */
 	heap_ptr = ALIGN_UP(heap_ptr, sizeof(uint32_t));
-	
 	top_heap_ptr = heap_ptr + HEAP_EXPAND_SIZE;
 	
 	uint32_t list_size = ((uint32_t)early_heap_top - early_heap_ptr) /
 			     sizeof(node_header_t *);
 	
-	init_sorted_list(&free_list, (void **)early_heap_ptr, list_size);
+	init_sorted_list(&free_node_list, (void **)early_heap_ptr, list_size);
 	
 	uint32_t heap_begin = heap_ptr + HEADER_SIZE;
 	uint32_t heap_end = top_heap_ptr;
@@ -435,11 +436,57 @@ void init_kheap()
 	heap_set_header(heap_ptr, 0, NULL, NULL, beginning_heap_size);
 	
 	/* Insert our first big node into the list */
-	insert_list((void *)heap_ptr, &free_list);
+	insert_list((void *)heap_ptr, &free_node_list);
 	
 	heap_list_end_ptr = heap_ptr;
 	
-	
 	kprint(INFO, "[INIT KHEAP] Heap ptr: %x Heap size: %d\n", 
 	       heap_ptr, beginning_heap_size);
+}
+
+void *kmalloc_page()
+{
+	unsigned int index = byte_index_get_free_slot(&page_list);
+	if (index == -1){
+		kprint(WARN, "Kmalloc_page: couldnt find free page\n");
+		return NULL;
+	}
+	
+	return (void *)(page_ptr + (index * PAGE_SIZE));
+}
+
+void kfree_page(void *ptr)
+{
+	if (!IS_ALIGNED((uint32_t)ptr, PAGE_SIZE)){
+		PANIC("kfree_page ptr not aligned");
+	}
+
+	uint32_t index = ((uint32_t)ptr - page_ptr) / PAGE_SIZE;
+	byte_index_release_tbl(index, &page_list);
+}
+	
+
+void init_page_heap()
+{
+	char *page_list_ptr = (char *)page_heap_bottom;
+	uint32_t page_num = ((uint32_t)page_heap_top - 
+			     (uint32_t)page_heap_bottom) / PAGE_SIZE;
+	page_num -= 1; //Use one page for the byte_index_list
+	
+	page_ptr = (uint32_t)page_heap_bottom + PAGE_SIZE;
+	/* Align up to page boundary just in case */
+	page_ptr = ALIGN_UP(page_ptr, PAGE_SIZE); 
+	
+	init_byte_index_list(page_list_ptr, page_num, &page_list);
+
+	kprint(INFO, "[INIT PAGE HEAP] Page ptr: %x Page number: %d\n", 
+	       page_ptr, page_num);
+
+}
+
+void init_heap()
+{
+	init_early_heap();
+	init_kheap();
+	init_page_heap();
 }
--- kernel/mm.c
+++ kernel/mm.c
@@ -4,145 +4,60 @@
 #include "../include/kdef.h"
 #include "../include/string.h"
 #include "../include/mm.h"
+#include "../include/byte_index_list.h"
 
+BYTE_INDEX_LIST_INIT(phys_mem_tbl);
 
-uint8_t *free_ptr;
-uint8_t *phys_mem_tbl;
 uint32_t phys_page_num;
-uint32_t tbl_size;
+extern int early_heap_ptr;
 
-extern void heap_bottom();
-extern int heap_ptr;
-
-unsigned int pages_mapped = 0;
-
-static inline uint32_t index_to_addr(unsigned int tbl_index, 
-			    unsigned int byte_index)
-{
-	return ((tbl_index * 8) + byte_index) * PAGE_SIZE;
-}
-
-static void update_free_ptr()
-{
-	uint8_t temp_entry = *free_ptr;
-	
-	while (free_ptr < (phys_mem_tbl + tbl_size)) {
-		if (temp_entry != 0xFF){
-			return;
-		}
-		temp_entry = *++free_ptr;
-	}
-
-	PANIC("WE COULDNT FIND ANY MORE FREE SPACE");
+static inline uint32_t index_to_addr(unsigned int index)
+{	
+	return (index * PAGE_SIZE);
 }
 
 void init_mem_manager(uint32_t phys_size)
 {
-	phys_mem_tbl = (uint8_t *)heap_ptr;
+	uint32_t tbl_size;
+	
 	phys_size = ALIGN_DOWN(phys_size, PAGE_SIZE);
 	phys_page_num = phys_size / PAGE_SIZE;
 	tbl_size = (phys_page_num / 8);
-	free_ptr = phys_mem_tbl;
-	memset(phys_mem_tbl, 0, tbl_size);
-	heap_ptr += tbl_size;
+	init_byte_index_list((char *)early_heap_ptr, tbl_size, &phys_mem_tbl);
+	
+	memset(phys_mem_tbl.array, 0, tbl_size);
+	early_heap_ptr += tbl_size;
 
-
-
-	kprint(INFO, "MM: Mem table start addr: %x\n", phys_mem_tbl);
+	kprint(INFO, "MM: Mem table start addr: %x\n", phys_mem_tbl.array);
 	kprint(INFO, "MM: Mem table size: %d bytes, mem phys_page_num: %x\n",
 	       tbl_size, phys_page_num);
 }
 
 void mem_set_tbl(uint32_t phys_addr) 
 {
-	unsigned int tbl_index;
-	unsigned int byte_index;
-	uint8_t temp_entry;
-
-	tbl_index = PHYS_ADDR_TO_PAGE_INDEX(phys_addr) / 8;
-	byte_index = PHYS_ADDR_TO_PAGE_INDEX(phys_addr) % 8;
-	
-	temp_entry = *(phys_mem_tbl + tbl_index);
-	
-	if ((temp_entry & (1 << byte_index)) == 0){
-		pages_mapped++;
-	}
-	
-	*(phys_mem_tbl + tbl_index) = (temp_entry | (1 << byte_index));
+	uint32_t index = PHYS_ADDR_TO_PAGE_INDEX(phys_addr);
+	byte_index_set_tbl(index, &phys_mem_tbl);
 }
 
 void mem_free_tbl(uint32_t phys_addr)
 {
-	unsigned int tbl_index;
-	unsigned int byte_index;
-	uint8_t temp_entry;
-
-	tbl_index = PHYS_ADDR_TO_PAGE_INDEX(phys_addr) / 8;
-	byte_index = PHYS_ADDR_TO_PAGE_INDEX(phys_addr) % 8;
-	
-	if (free_ptr > phys_mem_tbl + tbl_index)
-		free_ptr = (phys_mem_tbl + tbl_index);
-
-	
-	temp_entry = *(phys_mem_tbl + tbl_index);
-	
-	if ((temp_entry & (1 << byte_index)) != 0){ //Keeping track
-		pages_mapped--;
-	}
-
-	*(phys_mem_tbl + tbl_index) = (temp_entry & ~(1 << byte_index));
+	uint32_t index = PHYS_ADDR_TO_PAGE_INDEX(phys_addr);
+	byte_index_release_tbl(index, &phys_mem_tbl);
 }
 
 uint32_t mem_check_tbl(uint32_t phys_addr)
 {
-	unsigned int tbl_index;
-	unsigned int byte_index;
-	unsigned int ret;
-	uint8_t temp_entry;
-	
-	tbl_index = PHYS_ADDR_TO_PAGE_INDEX(phys_addr) / 8;
-	byte_index = PHYS_ADDR_TO_PAGE_INDEX(phys_addr) % 8;
-	
-	temp_entry = *(phys_mem_tbl + tbl_index);
-	ret = (temp_entry & (1 << byte_index));
-	ret = ret >> byte_index;
-	return ret;
+	uint32_t index = PHYS_ADDR_TO_PAGE_INDEX(phys_addr);
+	return byte_index_check_tbl(index, &phys_mem_tbl);
 }
 
 uint32_t mem_get_free_page_phys()
 {
-	uint8_t temp_entry;
-	uint32_t tbl_index;
-	uint32_t ret_addr;
+	uint32_t index = byte_index_get_free_slot(&phys_mem_tbl);
+	if (index == -1)
+		return -1;
 	
-	uint32_t byte_index = -1; //set so we know if we didnt change
-	unsigned int i = 0;
-
-	tbl_index =(uint32_t)(free_ptr - phys_mem_tbl);
-	if (phys_mem_tbl[tbl_index] == 0xFF) {
-		update_free_ptr();
-	}
-	
-	tbl_index =(uint32_t)(free_ptr - phys_mem_tbl);
-	if (phys_mem_tbl[tbl_index] == 0xFF) {
-		PANIC("MEM_TBL NOT FREE???");
-	}
-	
-	temp_entry = phys_mem_tbl[tbl_index];
-	for (i = 0; i < 8; i++) {
-		if ((temp_entry & (1 << i)) == 0) {
-			byte_index = i;
-			break;
-		}
-	}
-	
-	if (byte_index == -1) {
-		PANIC("BYTE_INDEX NOT SET");
-	}
-
-	ret_addr = index_to_addr(tbl_index, byte_index);
-	mem_set_tbl(ret_addr);
-	return ret_addr;
+	return (index * PAGE_SIZE);
 }
 
 void mem_release_page_phys(uint32_t phys_addr)
