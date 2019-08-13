4d10aa5002e32c2ed8f06fc86760913756dcc3bc
Luke Nowakowski-Krijger <lnowakow@eng.ucsd.edu>
2019-08-13 01:33:52 -0700

    Added useful linked list functions

--- include/linked_list.h
+++ include/linked_list.h
@@ -2,7 +2,7 @@
 #define _LINKED_LIST_H
 
 #define LINKED_LIST_INIT(list) \
-	static linked_list_t (list) = { NULL, NULL }
+	static linked_list_t (list) = { NULL, NULL, 0 }
 
 #define linked_list_for_each(pos, list) \
 	for (linked_list_node_t *(pos) = (list)->start_ptr; pos != NULL; pos = pos->next_ptr)
@@ -17,12 +17,18 @@ typedef struct linked_list_node {
 typedef struct linked_list {
 	linked_list_node_t *start_ptr;
 	linked_list_node_t *end_ptr;
+	unsigned int size;
 }linked_list_t;
 
-linked_list_t *linked_list_init();
+void linked_list_cycle(linked_list_t *list);
+int linked_list_enqueue(void *data, linked_list_t *list);
+void *linked_list_dequeue(linked_list_t *list);
+int linked_list_push(void *data, linked_list_t *list);
+void *linked_list_pop(linked_list_t *list);
+
 void *linked_list_get(unsigned int index, linked_list_t *list);
 int linked_list_search(void *data, linked_list_t *list);
-int linked_list_add(void *data, linked_list_t *list);
-void linked_list_remove(void *data, linked_list_t *list);
-
+int linked_list_add(void *data, unsigned int index, linked_list_t *list);
+int linked_list_remove(unsigned int index, linked_list_t *list);
+linked_list_t *linked_list_init();
 #endif
--- klib/linked_list.c
+++ klib/linked_list.c
@@ -11,7 +11,53 @@ linked_list_t *linked_list_init()
 	linked_list_t *list = kmalloc(sizeof(linked_list_t));
 	list->start_ptr = NULL;
 	list->end_ptr = NULL;
+	list->size = 0;
 	return list;
+}
+
+void linked_list_cycle(linked_list_t *list)
+{
+	linked_list_node_t *first_node = list->start_ptr;
+	linked_list_node_t *last_node = list->end_ptr;
+	
+	/* If this is the only node in the list or there
+	 * arent any nodes just return */
+	if (first_node == last_node)
+		return;
+	
+	linked_list_node_t *second_node = first_node->next_ptr;
+	
+	first_node->next_ptr = NULL;
+	first_node->prev_ptr = last_node;
+	last_node->next_ptr = first_node;
+	list->end_ptr = first_node;
+
+	second_node->prev_ptr = NULL;
+	list->start_ptr = second_node;
+}
+
+int linked_list_enqueue(void *data, linked_list_t *list)
+{
+	return linked_list_add(data, list->size, list);	
+}
+
+void *linked_list_dequeue(linked_list_t *list)
+{
+	void *ret = linked_list_get(0, list);
+	linked_list_remove(0, list);
+	return ret;
+}
+
+int linked_list_push(void *data, linked_list_t *list)
+{
+	return linked_list_add(data, 0, list);
+}
+
+void *linked_list_pop(linked_list_t *list)
+{
+	void *ret = linked_list_get(0, list);
+	linked_list_remove(0, list);
+	return ret;
 }
 
 void *linked_list_get(unsigned int index, linked_list_t *list)
@@ -41,25 +87,23 @@ int linked_list_search(void *data, linked_list_t *list)
 		return -1;
 	
 	linked_list_node_t *node = list->start_ptr;
-
+	int index = 0;
 	while (node != NULL){
 		if (node->data == data) {
-			return 0; //found
+			return index; //found
 		}
 		node = node->next_ptr;
+		index++;
 	}
 
-	return 1;
+	return -1;
 }
 
-int linked_list_add(void *data, linked_list_t *list)
+int linked_list_add(void *data, unsigned int index, linked_list_t *list)
 {
 	if (list == NULL) 
 		return -1;
 	
-	if (linked_list_search(data, list) == 0)
-		return 1; //already exists
-
 	linked_list_node_t *node = kmalloc(sizeof(linked_list_node_t));
 	memset(node, 0, sizeof(linked_list_node_t));
 	
@@ -70,37 +114,57 @@ int linked_list_add(void *data, linked_list_t *list)
 	if (list->start_ptr == NULL) {
 		list->start_ptr = node; //update list
 		list->end_ptr = node;
-	}
-	/* Otherwise update nodes around us */
-	else {
-		linked_list_node_t *last_node = list->end_ptr;
-		last_node->next_ptr = node; //update last_node
-		node->prev_ptr = last_node; //update new node
-		list->end_ptr = node; //update list
+		list->size++;
+		return 0;
 	}
 	
-	return 0;
+	/* Otherwise update nodes around us */
+	unsigned int temp_index = 0;
+	linked_list_node_t *temp_node = list->start_ptr;
+	
+	/* Search for the place we want to place this node */
+	while (temp_node != NULL && temp_index <= index) {
+		temp_node = temp_node->next_ptr;
+		temp_index++;
+	}
+	
+	/* Our node will be the last node in the list */
+	if (temp_node == NULL) {
+		linked_list_node_t *last_node = list->end_ptr;
+		list->end_ptr = node;
+		last_node->next_ptr = node;
+		node->prev_ptr = last_node;
+		list->size++;
+		return temp_index;
+	}
+	
+	/* our node is the first node in the list */
+	if (temp_node->prev_ptr == NULL)
+		list->start_ptr = node;
+
+	node->prev_ptr = temp_node->prev_ptr;
+	node->next_ptr = temp_node;
+	temp_node->prev_ptr = node;
+	
+	list->size++;
+	return temp_index;
 }
 
-void linked_list_remove(void *data, linked_list_t *list)
+int linked_list_remove(unsigned int index, linked_list_t *list)
 {
 	if (list == NULL)
-		return;
+		return -1;
 	
 	linked_list_node_t *node = list->start_ptr;
 	
-	/* Find node */
-	while (node != NULL) {
-		if(node->data == data) {
-			break;
-		}
-
+	unsigned int temp_index = 0;
+	while (node != NULL && temp_index <= index) {
 		node = node->next_ptr;
+		temp_index++;
 	}
-	
-	/* Node not found */
-	if (node == NULL) 
-		return; 
+
+	if (temp_index != index)
+		return -1;
 	
 	linked_list_node_t *prev_node = node->prev_ptr;
 	linked_list_node_t *next_node = node->next_ptr;
@@ -110,7 +174,8 @@ void linked_list_remove(void *data, linked_list_t *list)
 		list->start_ptr = NULL;
 		list->end_ptr = NULL;
 		kfree(node);
-		return;
+		list->size --;
+		return 0; //success
 	}
 
 	/* update previous node */
@@ -125,6 +190,8 @@ void linked_list_remove(void *data, linked_list_t *list)
 	/* Otherwise update list */
 	else
 		list->end_ptr = prev_node;	 
-
+	
+	list->size--;
 	kfree(node);
+	return 0;
 }
