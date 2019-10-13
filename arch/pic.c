0fc609981791f94b97875aab2dbc04c5e79ec769
Luke Nowakowski-Krijger <lnowakow@eng.ucsd.edu>
2019-10-13 15:41:04 -0700

    Updated various arch/* files to include more comments

--- arch/pic.c
+++ arch/pic.c
@@ -4,13 +4,6 @@
 #include "../include/io.h"
 #include "../include/pic.h"
 
-
-/*
-arguments:
-	offset1 - vector offset for master PIC
-		vectors on the master become offset1..offset1+7
-	offset2 - same for slave PIC: offset2..offset2+7
-*/
 void pic_remap()
 {
 	
@@ -26,24 +19,6 @@ void pic_remap()
         outb(0x21, 0x0);
         outb(0xA1, 0x0);  
 
-	/*
-	unsigned char a1, a2;
- 
-	a1 = inb(PIC1_DATA);                        // save masks
-	a2 = inb(PIC2_DATA);
- 
-	outb(PIC1_COMMAND, ICW1_INIT | ICW1_ICW4);  // starts the initialization sequence (in cascade mode)
-	outb(PIC2_COMMAND, ICW1_INIT | ICW1_ICW4);
-	outb(PIC1_DATA, offset1);                 // ICW2: Master PIC vector offset
-	outb(PIC2_DATA, offset2);                 // ICW2: Slave PIC vector offset
-	outb(PIC1_DATA, 4);                       // ICW3: tell Master PIC that there is a slave PIC at IRQ2 (0000 0100)
-	outb(PIC2_DATA, 2);                       // ICW3: tell Slave PIC its cascade identity (0000 0010)
- 
-	outb(PIC1_DATA, ICW4_8086);
-	outb(PIC2_DATA, ICW4_8086);
- 
-	outb(PIC1_DATA, a1);   // restore saved masks.
-	outb(PIC2_DATA, a2); */
 }
 
 void pic_sendEOI(unsigned char irq)
